742f799430e5c74f640dc3190dd79a44f1fea0f0
Adam Rush <adamrushuk@gmail.com>
2020-04-05 11:49:22 +0100

    Removed old code

--- manifests/nfs/nfs_test.sh
+++ manifests/nfs/nfs_test.sh
@@ -6,9 +6,8 @@
 kubectl create namespace media
 kubectl get all,pv,pvc -n media
 
-
 # TESTING NFS
-# create storage class
+# NOTE: storage class is NOT required for nfs on k3s
 # kubectl delete -f ./manifests/nfs/nfs_sc.yml
 # kubectl apply -f ./manifests/nfs/nfs_sc.yml
 # kubectl get sc
@@ -35,123 +34,17 @@ kubectl describe deploy/nfs-busybox -n media
 # check
 kubectl get all,pv,pvc -n media
 
+# check on node
+tree /mnt/
+ll /mnt/ssd/media
+cat /mnt/ssd/media/index.html
+tail -f /mnt/ssd/media/index.html
+
 # check within pod
-kubectl exec -it volume-debugger sh -n media
-ls -la /data
-touch /data/test_$(date +"%Y%m%d-%H%M%S")
-exit
+kubectl exec -it nfs-busybox-864d85c779-x5wjl sh -n media
+df -h
+ls -la /mnt/
+tail -f /mnt/localpod/index.html
 
 # [optional] cleanup
 kubectl delete pvc,pv --all
-
-
-
-
-
-
-
-
-
-
-# deploy persistent volume (pv)
-kubectl delete -f ./manifests/nfs/media.persistentvolume.yml
-kubectl apply -f ./manifests/nfs/media.persistentvolume.yml
-kubectl get pv
-kubectl describe pv media-ssd
-
-# create persistent volume claim (pvc)
-kubectl delete -f ./manifests/nfs/media.persistentvolumeclaim.yml
-kubectl apply -f ./manifests/nfs/media.persistentvolumeclaim.yml
-kubectl get pvc -n media
-kubectl describe pvc media-ssd
-
-# check
-kubectl get pv,pvc -A
-kubectl describe pv
-kubectl describe pvc -n media
-kubectl get sc
-
-
-# [optional] cleanup
-kubectl delete pvc,pv --all
-
-
-
-
-
-
-
-
-
-
-
-# create media ingress
-kubectl apply -f ./manifests/nfs/media.ingress.yml
-
-# check http://media.192.168.1.30.nip.io
-
-# heml repository - add bananaspliff
-helm repo add bananaspliff https://bananaspliff.github.io/geek-charts
-helm repo update
-
-# TODO: BitTorrent client - Transmission over VPN
-
-# TODO: Torrent Providers Aggregator- Jackett over VPN
-
-# TODO: TV Show Library Management - Sonarr
-# login to a worker node and create folder
-ssh pi@pi1
-sudo mkdir -p /mnt/ssd/media/configs/sonarr/
-tree /mnt/ssd/
-media.sonarr.values.yml
-
-
-# TODO: Movie Library Management - Radarr
-
-# ! WIP
-# media server - plex
-helm list -A
-helm uninstall plex --namespace media
-
-helm install plex charts/kube-plex/charts/kube-plex/ \
-  --values charts/values/media.plex.values.yml \
-  --namespace media --debug
-
-# check
-kubectl get all,pv,pvc -A
-kubectl get pods -n media -l app=kube-plex -o wide -w
-kubectl get services -n media -l app=kube-plex -o wide
-kubectl get deploy --namespace media --watch
-
-kubectl get events --sort-by=.metadata.creationTimestamp --namespace media
-kubectl get events --namespace media --watch
-
-kubectl get deploy -n media
-kubectl logs -n media -l app=kube-plex
-
-# ? Tshoot
-# show rendered helm template
-helm template plex charts/kube-plex/charts/kube-plex/ --values \
-  charts/values/media.plex.values.yml >plex_rendered.yml
-
-# show storage
-kubectl get pv,pvc -A
-kubectl describe pvc -A
-
-helm list -A
-
-kubectl describe deploy/plex-kube-plex -n media
-
-# debug
-kubectl delete -f ./manifests/nfs/pvc-debugger.yml
-kubectl apply -f ./manifests/nfs/pvc-debugger.yml
-kubectl exec -it volume-debugger sh -n media
-
-kubectl delete -f ./manifests/nfs/pvc-debugger2.yml
-kubectl apply -f ./manifests/nfs/pvc-debugger2.yml
-kubectl exec -it volume-debugger2 sh -n media
-
-cd /data
-ls -la
-vi test
-cat test
