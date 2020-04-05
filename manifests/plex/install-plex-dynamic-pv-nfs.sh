36c9259be622fe1c2b258469aa0fd4cdd52cf50b
Adam Rush <adamrushuk@gmail.com>
2020-04-05 16:21:40 +0100

    Added dynamic pv example with nfs provisioner

---
+++ manifests/plex/install-plex-dynamic-pv-nfs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Plex (dynamic PVs with NFS)
+
+# create namespace
+kubectl create namespace media
+
+# install nfs-client-provisioner for dynamic pv support
+# https://github.com/helm/charts/tree/master/stable/nfs-client-provisioner
+helm install nfs-provisioner stable/nfs-client-provisioner \
+    --set nfs.server=192.168.1.10 \
+    --set nfs.path=/mnt/ssd/media \
+    --set image.repository=quay.io/external_storage/nfs-client-provisioner-arm \
+    --set storageClass.name=nfs-client \
+    --namespace media --debug
+# misc values
+# --set nfs.mountOptions=
+
+# persistent volume claim (pvc) - uses dynamic pv
+kubectl apply -f ./manifests/plex/pvc-dynamic-plex.yml
+kubectl get pvc -n media
+kubectl describe pvc nfs -n media
+
+# install plex
+# https://github.com/munnerz/kube-plex
+helm install plex charts/kube-plex \
+  --values charts/values/media.plex.values.yml \
+  --namespace media --debug
+
+# check
+helm list -A
+kubectl get all,pv,pvc -n media
+
+# check files on node
+tree /mnt/
+ll /mnt/ssd/media
+
+# [optional] cleanup
+helm uninstall plex --namespace media
+kubectl delete -f ./manifests/plex/pvc-dynamic-plex.yml
+helm uninstall nfs-provisioner --namespace media
+# kubectl delete pvc,pv --all
